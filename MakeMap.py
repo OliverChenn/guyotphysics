d9fa00b5e32452be71e8983adebeab65ba81bc2c
oliverchen <chenoliver123@gmail.com>
2024-10-07 17:05:31 -0400

    removed specific labeled coordinates

--- MakeMap.py
+++ MakeMap.py
@@ -21,7 +21,6 @@ my_map.fillcontinents(color='coral')
 my_map.drawmapboundary()
 my_map.drawstates(color='b')
 
-# HERE YOU SHOULD READ EQDATA INSTEAD
 
 # These are the coordinates
 locations =[]
@@ -49,32 +48,8 @@ ys = [40.728, 40.716, 40.6915, 40.6978, 40.690, 40.6963, 40.7125]
 x1 = [-74.67]
 y1 = [40.728]
 
-x2 = [-74.757]
-y2 = [40.716]
-
-x3 = [-74.749]
-y3 = [40.6915]
-
-x4 = [-74.7463]
-y4 = [40.6978]
-
-x5 = [-74.7665]
-y5 = [40.690]
-
-x6 = [-74.7596]
-y6 = [40.6963]
-
-x7 = [-74.757]
-y7 = [40.7125]
-
 # AGAIN LOOP, AND USE THE THIRD COLUMN OF THE EARTHQUAKE COORDINATES
 plt.plot(x1, y1, 'blue', label='2.6')
-plt.plot(x2, y2, 'red', label='2.9')
-plt.plot(x3, y3, 'green', label='2.6')
-plt.plot(x4, y4, 'cyan', label='3.7')
-plt.plot(x5, y5, 'magenta', label='2.6')
-plt.plot(x6, y6, 'yellow', label='4.8')
-plt.plot(x7, y7, 'black', label='2.9')
 
 
 plt.title('Earthquake occurrence between April and May\nin New Jersey ')

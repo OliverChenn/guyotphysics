3a24f94f56aac67515848e58ff36236b78a4fae0
oliverchen <chenoliver123@gmail.com>
2024-11-25 17:00:56 -0500

    a

--- ReadCatalog.py
+++ ReadCatalog.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
+#defines a function
 def make_df(fname):
+    #reads the csv file
     df = pd.read_csv(fname)
+    #calls on these 3 values
     lat, lon, magnitude = df['latitude'], df['longitude'], df['mag']
-    return lat, lon, magnitude
-    print(lat, lon, magnitude)
 
     return lat, lon, magnitude
 

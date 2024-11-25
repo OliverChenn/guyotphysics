656d7de355c5a097c4bdac4bb6ce22e6a9b3fcbf
oliverchen <chenoliver123@gmail.com>
2024-11-25 16:56:09 -0500

    a

--- ReadCatalog.py
+++ ReadCatalog.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
+df = pd.read_csv(fname)
 
-df = pd.read_csv('/Users/oliverchen/PyCharmProjects/guyotphysics/DATA/eqdata1.csv'
-)
 def make_df(fname):
     df = pd.read_csv(fname)
     lat, lon, magnitude = df['latitude'], df['longitude'], df['mag']

9b30f03159a0b7d8943f4273dad5fc01201de44c
oliverchen <chenoliver123@gmail.com>
2024-12-23 09:40:17 -0500

    a

--- ReadCatalog.py
+++ ReadCatalog.py
@@ -4,6 +4,6 @@ def make_df(fname):
     # Reads the csv file
     df = pd.read_csv(fname)
     # Extracts various values - get out the time also
-    lat, lon, magnitude = df['latitude'], df['longitude'], df['mag']
+    lat, lon, magnitude, time = df['latitude'], df['longitude'], df['mag'], df['time']
 
-    return lat, lon, magnitude
+    return lat, lon, magnitude, time

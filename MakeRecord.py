67d211b58c6d242ac1ace0ff00bf2de1dbbd80ed
oliverchen <chenoliver123@gmail.com>
2025-01-22 17:54:55 -0500

    Query

--- MakeRecord.py
+++ MakeRecord.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-import glob
-from obspy.core import read
-for file in glob.glob('*.z'):
-    st = read(file)
-    tr = st[0]
-    msg = "%s %s %f %f" % (tr.stats.station, str(tr.stats.starttime),
-                           tr.data.mean(), tr.data.std())
-    print(msg)
+from obspy import read
+st = read("/Users/oliverchen/PyCharmProjects/guyotphysics/DATA/S0001_MC-PH1_0248_20240405_142022.seed")
+print(st)
+st.plot()
